6284bb9d91626bb8691c3850a9d69cbda881f68c
MarceloRebolledo <m.rebolledo17@ufromail.cl>
2024-11-15 12:12:54 -0300

    despues del refactor

--- src/main/java/org/example/Main_Kaprekar.java
+++ src/main/java/org/example/Main_Kaprekar.java
@@ -2,6 +2,7 @@ package org.example;
 // -encoding UTF-8 -charset UTF-8 -docencoding UTF-8
 
 
+import java.util.Arrays;
 
 public class Main_Kaprekar {
 
@@ -10,9 +11,19 @@ public class Main_Kaprekar {
 
     }
 //aquí van sus funciones:
+    public static int kaprekarOp(int num) {
+        int n1 = num / 1000; //numero de miles
+        int n2 = (num / 100) % 10; //numero de centena
+        int n3 = (num / 10) % 10; //numero de decena
+        int n4 = num % 10; //numero de unidad
 
-    //solo para pasar el test
-    public static int kaprekarOp(int n) {
-        return 3087;
+        int[] numeros = {n1, n2, n3, n4};
+        Arrays.sort(numeros);
+
+        int asc = numeros[0] * 1000 + numeros[1] * 100 + numeros[2] * 10 + numeros[3];
+        int desc = numeros[3] * 1000 + numeros[2] * 100 + numeros[1] * 10 + numeros[0];
+
+        return desc - asc;
     }
+
 }
