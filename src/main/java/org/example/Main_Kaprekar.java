0e66290ea02e4f088257ced0e432ebf9d9d8ea07
MarceloRebolledo <m.rebolledo17@ufromail.cl>
2024-11-15 11:36:07 -0300

    creacion main y test

---
+++ src/main/java/org/example/Main_Kaprekar.java
@@ -0,0 +1,15 @@
+package org.example;
+// -encoding UTF-8 -charset UTF-8 -docencoding UTF-8
+
+
+
+public class Main_Kaprekar {
+
+    public static void main(String[] args){
+        System.out.println("Clase Main");
+
+    }
+
+//aquí van sus funciones:
+
+}
